e95d70f56c251111c54f745d022d10d8672130e1
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-07-07 13:13:17 +0200

    Errores 404 y general, e index.js

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,11 @@
+const morganFreeman = require("morgan");
 const app = require("./init");
+const rutasAnimales = require("./rutas/animales");
+const rutasUsuarios = require("./rutas/usuarios");
+const { error404, errorGeneral } = require("./errores");
+
+app.use(morganFreeman("dev"));
+app.use("/animales", rutasAnimales);
+app.use("/usuarios", rutasUsuarios);
+app.use(error404);
+app.use(errorGeneral);
